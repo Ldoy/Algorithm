6c8e399a990b8cbaec78203ca79bdaf5e86ae089
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2021-11-22 23:23:44 +0900

    refactor: Change logic

--- HackerRank/Algoritms/WarmUp/CompareTriplets.swift
+++ HackerRank/Algoritms/WarmUp/CompareTriplets.swift
@@ -1,12 +1,13 @@
 func compareTriplets(a: [Int], b: [Int]) -> [Int] {
     var scoreA = 0
-    var equal = 0
+    var scoreB = 0
     for i in 0...2 {
         if a[i] > b[i] {
             scoreA += 1
-        } else if a[i] == b[i] {
-            equal += 1
+        } else if a[i] < b[i] {
+            scoreB += 1
         }
     }
-    return [scoreA, (3 - scoreA - equal)]
+    return [scoreA, scoreB]
 }
+
