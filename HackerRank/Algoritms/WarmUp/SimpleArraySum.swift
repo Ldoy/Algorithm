86db4f941c735915133ed9df50cbcd7c695ab8d3
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2021-11-22 23:20:29 +0900

    feat: SimpleArraySum

---
+++ HackerRank/Algoritms/WarmUp/SimpleArraySum.swift
@@ -0,0 +1,6 @@
+func simpleArraySum(ar: [Int]) -> Int {
+    // Write your code here
+    return  ar.reduce(0, +)
+}
+
+// https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
